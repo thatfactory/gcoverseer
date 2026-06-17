4e8d863bccf52a5302630524341f3bb3845352a6
Fernando Fernandes <fernando.fernandes@blacklane.com>
2026-06-17 13:54:16 +0200

    Update AppLogger to 1.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/thatfactory/applogger",
-            from: "0.1.0"
+            from: "1.0.0"
         ),
         .package(
             url: "https://github.com/apple/swift-testing.git",
